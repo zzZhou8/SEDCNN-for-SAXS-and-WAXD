ddd404cedff71c3deb4a476f7790d489581416d0
zzZhou8 <89622113+zzZhou8@users.noreply.github.com>
2022-10-20 16:40:21 +0800

    Update loader.py

--- SEDCNN4_SAXS/loader.py
+++ SEDCNN4_SAXS/loader.py
@@ -4,10 +4,9 @@ import numpy as np
 from torch.utils.data import DataLoader,Dataset
 import random
 
-#这里的测试方式还是比较属于自导自演的，因为是自己测试自己的未知，用以评估系统自身的降噪能力
 class SAXS_dataset(Dataset):
     def __init__(self,mode,saved_path,patch_n=None,patch_size=None):
-        assert mode in ['train', 'test'], "mode is 'train' or 'test'"#确定现在是在测试还是在训练
+        assert mode in ['train', 'test'], "mode is 'train' or 'test'"
         
         train_input_path=os.path.join(saved_path,'SAXS_train_dot3.npy')
         train_target_path=os.path.join(saved_path,'SAXS_train_dot10.npy')
@@ -20,8 +19,7 @@ class SAXS_dataset(Dataset):
         self.patch_size = patch_size
 
         if self.mode=='train':
-            #这里是把最后的两组图片完全拿来测试了，用以测试模型的降噪能力是否合乎规范
-            self.input_=np.load(train_input_path)##这就是纯粹因为网络架构的原因妥协了576=32*18,所以遭得住这么折腾
+            self.input_=np.load(train_input_path)
             self.target_=np.load(train_target_path)
 
         else: # self.mode =='test'
@@ -43,27 +41,27 @@ class SAXS_dataset(Dataset):
 
             if self.patch_size:
                 input_patch,target_patch=get_patch(input_img,target_img,self.patch_n,self.patch_size)
-                return (input_patch,target_patch)#训练时是返回切割的数据的
+                return (input_patch,target_patch)
             else:
-                return(input_img,target_img)#test的时候是原封不动的返回的
+                return(input_img,target_img)
 
         else: # mode =='test'
 
             input_img=self.input_[idx]
             target_img=self.target_[idx]
 
-            return(input_img,target_img)#test的时候是原封不动的返回的
-def get_patch(full_input_img,full_target_img,patch_n,patch_size):#10,64
-    assert full_input_img.shape == full_target_img.shape#尺寸要对的上才行
+            return(input_img,target_img)
+def get_patch(full_input_img,full_target_img,patch_n,patch_size):
+    assert full_input_img.shape == full_target_img.shape
     patch_input_imgs=[]
     patch_target_imgs=[]
     h, w = full_input_img.shape
-    new_h, new_w = patch_size, patch_size#决定了裁切后的图片有多大
-    for _ in range(patch_n):#决定要产生多少幅64*64的训练用图片
-        top=np.random.randint(0,h-new_h)#在这两个闭区间中取整数
+    new_h, new_w = patch_size, patch_size
+    for _ in range(patch_n):
+        top=np.random.randint(0,h-new_h)
         left = np.random.randint(0, w-new_w)
         patch_input_img = full_input_img[top:top+new_h, left:left+new_w]
-        patch_target_img = full_target_img[top:top+new_h, left:left+new_w]#抽取的很随机嘛，但是数量有限
+        patch_target_img = full_target_img[top:top+new_h, left:left+new_w]
         patch_input_imgs.append(patch_input_img)
         patch_target_imgs.append(patch_target_img)
     return np.array(patch_input_imgs), np.array(patch_target_imgs)
