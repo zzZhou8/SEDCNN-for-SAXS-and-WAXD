a9bf2013c296f7f982237a139a2a8ca8a5bb482c
zzZhou8 <89622113+zzZhou8@users.noreply.github.com>
2022-10-20 16:42:15 +0800

    Update main.py

--- SEDCNN4_SAXS/main.py
+++ SEDCNN4_SAXS/main.py
@@ -7,7 +7,7 @@ from solver import Solver
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--mode', type=str, default='train')
-parser.add_argument('--Loop_test', type=bool, default=False)#循环测试以绘制走势图
+parser.add_argument('--Loop_test', type=bool, default=False)#Test all the saved models
 parser.add_argument('--saved_path', type=str, default='./npy_img/')
 parser.add_argument('--save_path', type=str, default='./save/')
 parser.add_argument('--result_fig', type=bool, default=True)
@@ -18,11 +18,11 @@ parser.add_argument('--trunc_min', type=float, default=0)
 parser.add_argument('--trunc_max', type=float, default=255)
 
 
-parser.add_argument('--patch_n', type=int, default=10)#一个图进行多少次随机切割，在随机切割时这个数最大为6，如果是大小切的话就是1没多的！
+parser.add_argument('--patch_n', type=int, default=10)
 parser.add_argument('--patch_size', type=int, default=55)
 parser.add_argument('--batch_size', type=int, default=16)
 
-parser.add_argument('--num_epochs', type=int, default=505)#决定训练轮次
+parser.add_argument('--num_epochs', type=int, default=505)
 parser.add_argument('--print_iters', type=int, default=20)
 parser.add_argument('--decay_epochs', type=int, default=20)
 parser.add_argument('--save_epochs', type=int, default=20)
@@ -38,31 +38,31 @@ args = parser.parse_args(args=[])
 
 def main(args):
     
-    if not os.path.exists(args.save_path):#没有就产生一个
+    if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
         print('Create path : {}'.format(args.save_path))
 
-    if args.result_fig:#专门存结果的地方
+    if args.result_fig:
         fig_path = os.path.join(args.save_path, 'fig')
         if not os.path.exists(fig_path):
             os.makedirs(fig_path)
             print('Create path : {}'.format(fig_path))
 
-    print('正在准备数据啦')
+    print('Preparing data')
     data_loader = get_loader(mode=args.mode,#默认为train
                              saved_path=args.saved_path,#'./npy_img/'
-                             patch_n=(args.patch_n if args.mode=='train' else None),#
+                             patch_n=(args.patch_n if args.mode=='train' else None),
                              patch_size=args.patch_size,#
-                             batch_size=(args.batch_size if args.mode=='train' else 1),#训练时为16，测试时为1
+                             batch_size=(args.batch_size if args.mode=='train' else 1),
                              )
-    print('数据准备好啦')
+    print('The data is ready')
     solver = Solver(args, data_loader)
-    print('solver准备好了')
+    print('Solver is ready')
     if args.mode == 'train':
-        print('开始训练啦')
+        print('Lets start training.')
         solver.train()
     elif args.mode == 'test':
-        print('开始测试啦')
+        print('Lets start testing.')
         solver.test()
 
 main(args)
